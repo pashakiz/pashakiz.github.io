6f4312f411b44a848c3087ce1a47c673ae9b802f
pashakiz <pashakiz@gmail.com>
2018-01-22 10:57:36 +0300

    test php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Заголовок</title>
+	<meta name="description" content="" />
+	<style>
+		html, body {
+			height: 100%;
+			margin: 0;
+		}
+		body {
+		  font-size: 15px;
+		  background-color: #2F2727;
+		  background: -webkit-gradient(radial, center center, 0, center center, 460, from(#1a82f7), to(#2F2727));
+		  background: -webkit-radial-gradient(circle, #1a82f7, #2F2727);
+		  background: -moz-radial-gradient(circle, #1a82f7, #2F2727);
+		  background: -ms-radial-gradient(circle, #1a82f7, #2F2727);
+		}
+		.text {
+		  color: #fff;
+		  font-family: Helvetica, sans-serif;
+		  font-style: italic;
+		}
+		table {}
+	</style>
+</head>
+<body>
+	<table style="height: 100%; width: 100%;">
+		<tbody>
+			<tr>
+				<td align="center" valign="center">
+					<div class="text">pashakiz.github.io</div>
+					<div class="text"><?php echo 'php test';?></div>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</body>
+</html>
