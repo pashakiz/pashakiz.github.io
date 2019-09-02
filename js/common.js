d128028282d958be026e4160958ba6864512a03e
pashakiz <pashakiz@gmail.com>
2019-09-02 20:00:50 +0300

    index start

--- js/common.js
+++ js/common.js
@@ -8,14 +8,34 @@ function disableStylesheet (node) {
 
 var themelight = document.getElementById('light');
 var themedark = document.getElementById('dark');
-disableStylesheet(themelight);
-
-function toggleStylesheets() {
-    if (themelight.disabled) {
-        disableStylesheet(themedark);
-        enableStylesheet(themelight);
-    } else {
-        disableStylesheet(themelight);
-        enableStylesheet(themedark);
-    }
+if( $('html').hasClass('themedark') ) {
+    turnDarkTheme();
 }
+if( $('html').hasClass('themelight') ) {
+    turnLightTheme();
+}
+
+function turnLightTheme() {
+    $('html').removeClass('themedark');
+    $('html').addClass('themelight');
+    $('.cssmode__icon').removeClass('icon-sun');
+    $('.cssmode__icon').addClass('icon-moon');
+    disableStylesheet(themedark);
+    enableStylesheet(themelight);
+}
+function turnDarkTheme(){
+    $('html').removeClass('themelight');
+    $('html').addClass('themedark');
+    $('.cssmode__icon').removeClass('icon-moon');
+    $('.cssmode__icon').addClass('icon-sun');
+    disableStylesheet(themelight);
+    enableStylesheet(themedark);
+}
+
+$('.cssmode__icon').on('click', function(){
+    if( $('html').hasClass('themedark') ) {
+        turnLightTheme();
+    } else {
+        turnDarkTheme();
+    }
+});
