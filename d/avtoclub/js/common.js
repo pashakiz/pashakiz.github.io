c1a1c30efce69cdc5f5e9a397ce5e6cfc7dc1581
pashakiz <pashakiz@gmail.com>
2019-09-09 22:31:27 +0300

    update d

---
+++ d/avtoclub/js/common.js
@@ -0,0 +1,16 @@
+$('.exam__tab').on('click', function(){
+    $('.exam__tab').removeClass('active');
+    $(this).addClass('active');
+    if ( $(this).hasClass('exam__tab_topics') ) {
+        $('.exam__content').removeClass('active');
+        $('.exam__content_topics').addClass('active');
+    }
+    if ( $(this).hasClass('exam__tab_numbers') ) {
+        $('.exam__content').removeClass('active');
+        $('.exam__content_numbers').addClass('active');
+    }
+    if ( $(this).hasClass('exam__tab_exam') ) {
+        $('.exam__content').removeClass('active');
+        $('.exam__content_exam').addClass('active');
+    }
+});
