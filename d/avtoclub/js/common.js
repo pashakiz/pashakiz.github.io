17618c85e66380c809072687698436761811704a
pashakiz <pashakiz@gmail.com>
2019-09-10 01:42:35 +0300

    update d

--- d/avtoclub/js/common.js
+++ d/avtoclub/js/common.js
@@ -1,16 +1,25 @@
-$('.exam__tab').on('click', function(){
-    $('.exam__tab').removeClass('active');
-    $(this).addClass('active');
-    if ( $(this).hasClass('exam__tab_topics') ) {
-        $('.exam__content').removeClass('active');
-        $('.exam__content_topics').addClass('active');
-    }
-    if ( $(this).hasClass('exam__tab_numbers') ) {
-        $('.exam__content').removeClass('active');
-        $('.exam__content_numbers').addClass('active');
-    }
-    if ( $(this).hasClass('exam__tab_exam') ) {
-        $('.exam__content').removeClass('active');
-        $('.exam__content_exam').addClass('active');
-    }
+$(document).ready(function(){
+
+    //tabs
+    $('.exam__tab').on('click', function(){
+        $('.exam__tab').removeClass('active');
+        $(this).addClass('active');
+        if ( $(this).hasClass('exam__tab_topics') ) {
+            $('.exam__content').removeClass('active');
+            $('.exam__content_topics').addClass('active');
+        }
+        if ( $(this).hasClass('exam__tab_numbers') ) {
+            $('.exam__content').removeClass('active');
+            $('.exam__content_numbers').addClass('active');
+        }
+        if ( $(this).hasClass('exam__tab_exam') ) {
+            $('.exam__content').removeClass('active');
+            $('.exam__content_exam').addClass('active');
+        }
+    });
+
+    //button to up
+    $('#gotop').on('click', function () {
+        $('body,html').animate({scrollTop: 0}, 800);
+    });
 });
