f7cf8dd8e639e718c89f15fedc0181767cd2c022
pashakiz <pashakiz@gmail.com>
2019-09-22 02:03:53 +0300

    update d (avtoclub)

--- d/avtoclub/js/grabdata.js
+++ d/avtoclub/js/grabdata.js
@@ -45,3 +45,41 @@ for (var i = 0; i < quest.length; i++) {
 
 questions.shift();
 console.log( JSON.stringify(questions, null, 4) );
+
+/*========================================================================*/
+// поиск дублей
+function checkDoubles() {
+	var indexes = [];
+	var res = [{text:'text', indexes: [0,0,0]}];
+	for (var i = 0; i < questions.length-1; i++) {
+        for (var j = 0; j < questions.length-1; j++) {
+        	if (i != j) {
+                if (questions[i].text == questions[j].text) {
+                    indexes.push(j);
+				}
+			}
+		}
+		if (indexes.length) {
+            indexes.unshift(i);
+            res.push({
+            	text: questions[i].text,
+				indexes: indexes
+			});
+		}
+        indexes = [];
+	}
+    console.log( JSON.stringify(res, null, 1) );
+}
+
+
+/*=================================================================*/
+// поиск дублей
+var questionsStrs = [];
+for (var i = 0; i < questions.length-1; i++) {
+    questionsStrs.push(questions[i].text);
+}
+var testres = questionsStrs.filter(function (elem, pos, arr) {
+    return pos !== arr.indexOf(elem) || pos !== arr.lastIndexOf(elem);
+});
+console.log(testres);
+/*=================================================================*/
