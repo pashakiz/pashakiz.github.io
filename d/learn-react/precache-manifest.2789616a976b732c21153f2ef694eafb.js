d9538648c91e1aeeea3a7df3fa5c7508346172d0
pashakiz <pashakiz@gmail.com>
2019-09-27 22:41:13 +0300

    add learn-react build

---
+++ d/learn-react/precache-manifest.2789616a976b732c21153f2ef694eafb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c378516e294cc28058225866685c78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7822329afdef9db90bc",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "1aeefa623e6603cb0ee6",
+    "url": "/static/css/main.2480ad34.chunk.css"
+  },
+  {
+    "revision": "d7822329afdef9db90bc",
+    "url": "/static/js/2.aee3bc7a.chunk.js"
+  },
+  {
+    "revision": "1aeefa623e6603cb0ee6",
+    "url": "/static/js/main.b191c279.chunk.js"
+  },
+  {
+    "revision": "ec16e2010ded332531ff",
+    "url": "/static/js/runtime-main.2bda1fdc.js"
+  }
+]);
