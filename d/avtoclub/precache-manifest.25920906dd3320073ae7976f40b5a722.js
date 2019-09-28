c5e56d2992dedb7c89530cab5f7033a001a34af4
pashakiz <pashakiz@gmail.com>
2019-09-28 04:27:16 +0300

    add avtoclub exam-react init build

---
+++ d/avtoclub/precache-manifest.25920906dd3320073ae7976f40b5a722.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf8fbde4f05dd46e3b79d6711c35ae9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f938aca7c52b243a5036",
+    "url": "/static/css/main.206c3fae.chunk.css"
+  },
+  {
+    "revision": "5694e4cb9b10be3e064b",
+    "url": "/static/js/2.6966c4cc.chunk.js"
+  },
+  {
+    "revision": "f938aca7c52b243a5036",
+    "url": "/static/js/main.d6a94718.chunk.js"
+  },
+  {
+    "revision": "543670b05d08d021372a",
+    "url": "/static/js/runtime-main.db8ead41.js"
+  }
+]);
