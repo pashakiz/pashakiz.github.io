7679f5ef98843066f9c7945b7a17908a0da996a0
pashakiz <pashakiz@gmail.com>
2020-02-19 05:48:19 +0300

    add kizernis.dn.ua

---
+++ d/kizernis.dn.ua/js/map.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+	function initialize() {
+        var mapOptions = {
+          center: new google.maps.LatLng(-34.397, 150.644),
+          zoom: 8
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+    }
+    
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+	$('#map-canvas').on('shown', function () {
+ 		google.maps.event.trigger(map, 'resize');
+	});
+});
